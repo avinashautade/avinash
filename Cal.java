c2541eaf1d30c0a4eabdf0aaf9c0cbf0e94e84bd
avinash <autadeavi111@gmail.comcls>
2023-05-17 16:39:41 +0530

    java program

---
+++ Cal.java
@@ -0,0 +1,13 @@
+class Add {
+
+     void sum () {
+       int a=12;
+       int b=15;
+    
+       int c= a+b;
+   System.out.println(c);
+
+    }
+
+    
+}
---
+++ Test.java
@@ -0,0 +1,10 @@
+class Test{
+
+  public static void main (String [] args)
+     {
+         Add a = new Add();
+     
+          a.sum();
+
+     }
+}
